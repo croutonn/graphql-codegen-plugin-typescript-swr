bfbdddcfbfd2ebee5fd34cd1a18566f77e1910ae
Eito Katagiri <eitoball@gmail.com>
2021-10-30 10:51:28 +0900

    format with prettier

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,11 @@ export const plugin: PluginFunction<RawSWRPluginConfig> = (
   const allAst = concatAST(documents.map((v) => v.document))
 
   const allFragments: LoadedFragment[] = [
-    ...(allAst.definitions.filter(
-      (d) => d.kind === Kind.FRAGMENT_DEFINITION
-    ) as FragmentDefinitionNode[]).map((fragmentDef) => ({
+    ...(
+      allAst.definitions.filter(
+        (d) => d.kind === Kind.FRAGMENT_DEFINITION
+      ) as FragmentDefinitionNode[]
+    ).map((fragmentDef) => ({
       node: fragmentDef,
       name: fragmentDef.name.value,
       onType: fragmentDef.typeCondition.name.value,
